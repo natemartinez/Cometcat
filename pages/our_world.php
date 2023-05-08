2c7b46fff0b1d0a19ac2b9da59342d2846b602e2
natemartinez <nathanielmartinez2001@gmail.com>
2023-05-08 12:56:10 -0500

    added parts to "our world"

--- pages/our_world.php
+++ pages/our_world.php
@@ -38,15 +38,7 @@
                 <li>
                     <a href="../index.php">Home</a>
                 </li>
-                <li>       
-                    <a class='dropdown-btn' id="firstItem" href="#">Our World<i class="fa fa-caret-down"></i></a>
-                    
-                    <div class="dropdown-container" id='ow_dropdown'>
-                      <a href="#">Overview</a>
-                      <a href="#">Phenomenons</a>
-                      <a href="#">Fun Facts</a>
-                    </div>
-                </li>
+  
                 <li>
                     <a id="secondItem" href="space_neighbors.php">Our Space Neighbors</a> 
                 </li>
@@ -84,8 +76,8 @@
                 <div id='mid_square'></div>
                 <div id='btm_square'></div>
                 <img id='hills_img' src="../images/field.jpg" alt="hills">
-                <img id='desert_img' src="../images/ocean.jpg" alt="desert">
-                <img id='mountains_img' src="../images/clouds.jpg" alt="mountains">
+                <img id='water_img' src="../images/ocean.jpg" alt="desert">
+                <img id='clouds_img' src="../images/clouds.jpg" alt="mountains">
                 <div id='header_pg'>
                   <h1>The Blue Marble</h1>
                 </div>
@@ -98,18 +90,52 @@
              </div>
 
              <div class='gl_content_box'>
+                  <p id='header_1'>Why is the Sun so important?</p>
                  <p id='gl_first_text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur eos dignissimos aliquid assumenda libero tenetur minus perspiciatis, incidunt cupiditate quas expedita soluta deleniti officiis odio. At inventore veniam dolorum pariatur?</p>
                  <img id='sunrise' src="../images/sunrise.jpg" alt="sunrise">  
                  <img id='soup_pic' src="../images/soup.jpg" alt="soup">
+                 <p id='header_2'> What's the Goldilocks Zone?</p>
                  <p id='gl_second_text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi deserunt ducimus odio at inventore unde! Culpa eum similique nemo sunt. Quas, eligendi? Iure a labore suscipit delectus blanditiis aut saepe.</p>  
                           
              </div>
 
-             <div class='life_content'>
-               <div></div>
-             </div>
+
              
-           </div>                                                                          
+             <div class='life_content'>
+              <div id='bg_square'></div>
+              <img id='ocean_floor' src="../images/ocean_floor.jpg" alt="ocean floor">
+              <img id='mountain' src="../images/mountains_ow.jpg" alt="snowy mountain">  
+              <img id='rainforest' src="../images/rainforest.jpg" alt="rainforest">
+              <img id='arizona' src="../images/arizona.jpg" alt="arizona scenery">
+              <div id='life_content_text'>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum magnam dignissimos atque blanditiis natus temporibus illo dolores, expedita facere consectetur minima rerum tenetur iure ea quidem provident modi odit aut?</p>
+              </div>
+             </div>
+
+             <div class='moon_content'>
+               <p id='moon_content_text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci exercitationem provident ex sequi minima quo officia error facere itaque omnis, corrupti ipsam possimus veritatis deserunt architecto laboriosam commodi voluptates optio.</p>
+               <button id='moon_button'>Check it out!</button>
+               <div id='bg_diamond'></div>
+               <div id='bg_diamond_2'></div>
+               <img id='moon_pic' src="../images/moon.png" alt="the moon">
+               <img id='cat_logo_moon' src="../images/logo_cat.png" alt="cat jumping">
+               </div>
+ 
+             <div class='end_content'>
+              <img id='n_lights_img' src="../images/nLights.jpg" alt="northern lights">
+              <div id='end_img_square_one'></div>
+              <div id='end_img_square_two'></div>
+              <div id='end_text_square'>
+                <div id='end_text'>
+                 <h1>We still have more to learn!</h1>
+                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime totam asperiores quae facere qui iure obcaecati impedit sapiente autem. Laboriosam dolorem quae ea quaerat omnis! Ab eius corrupti libero quam?</p>
+                </div>
+                
+              </div>
+             </div>
+          </div> 
+           
+             
           <div class="container">
                 <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
                   <div class="col mb-3">
